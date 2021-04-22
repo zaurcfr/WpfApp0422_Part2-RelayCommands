7b727074b5d8f666ea2da9d784d67736f4e67457
Cefe_a8xp@itstep.edu.az <Cefe_a8xp@itstep.edu.az>
2021-04-22 17:52:07 +0400

    Add project files.

---
+++ WpfApp0422_Part2 RelayCommands/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp0422_Part2_RelayCommands.Command;
+
+namespace WpfApp0422_Part2_RelayCommands
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public RelayCommand MessageCommand { get; set; }
+        public RelayCommand SendCommand { get; set; }
+        public MainWindow()
+        {
+            this.DataContext = this;
+            MessageCommand = new RelayCommand(Display);
+            SendCommand = new RelayCommand(Send);
+
+            InitializeComponent();
+        }
+
+        public void Display(object text = null)
+        {
+            MessageBox.Show("Display");
+        }
+        public void Send(object text = null)
+        {
+            MessageBox.Show("Send");
+        }
+
+
+    }
+}
